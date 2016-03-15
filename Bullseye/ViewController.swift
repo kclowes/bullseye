4aa70b5e8358de75b86a8c8fb5f1d92723114908
Keri Clowes <kclowes@quickleft.com>
2016-03-15 13:46:57 -0600

    Remove Whitespace

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -14,33 +14,33 @@ class ViewController: UIViewController {
     var targetValue = 0
     var score = 0
     var round = 0
-    
+
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var roundLabel: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
         slider.setThumbImage(thumbImageNormal, forState: .Normal)
-        
+
         let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")
         slider.setThumbImage(thumbImageHighlighted, forState: .Highlighted)
-        
+
         let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
-        
+
         if let trackLeftImage = UIImage(named: "SliderTrackLeft") {
             let trackLeftResizable = trackLeftImage.resizableImageWithCapInsets(insets)
             slider.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
         }
-        
+
         if let trackRightImage = UIImage(named: "SliderTrackRight") {
             let trackRightResizable = trackRightImage.resizableImageWithCapInsets(insets)
             slider.setMaximumTrackImage(trackRightResizable, forState: .Normal)
         }
-        
+
         startNewGame()
         updateLabels()
     }
@@ -52,9 +52,9 @@ class ViewController: UIViewController {
     @IBAction func showAlert() {
         let difference = abs(currentValue - targetValue)
         let points = 100 - difference
-        
+
         score += points
-        
+
         if difference == 0 {
             title = "Perfect"
         } else if difference < 10 {
@@ -62,9 +62,9 @@ class ViewController: UIViewController {
         } else {
             title = "Not even close :-("
         }
-        
+
         let message = "You scored \(points) points!"
-        
+
         let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
         let action = UIAlertAction(title: "Ok!", style: .Default, handler: {
             action in
@@ -74,11 +74,11 @@ class ViewController: UIViewController {
         alert.addAction(action)
         presentViewController(alert, animated: true, completion: nil)
     }
-    
+
     @IBAction func sliderMoved(slider: UISlider) {
         currentValue = lroundf(slider.value)
     }
-    
+
     @IBAction func startNewGame() {
         score = 0
         round = 0
@@ -95,14 +95,14 @@ class ViewController: UIViewController {
         transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
         view.layer.addAnimation(transition, forKey: nil)
     }
-    
+
     func startNewRound() {
         currentValue = lroundf(slider.value)
         targetValue = 1 + Int(arc4random_uniform(100))
         slider.value = Float(currentValue)
         round += 1
     }
-    
+
     func updateLabels() {
         targetLabel.text = String(targetValue)
         scoreLabel.text = String(score)
